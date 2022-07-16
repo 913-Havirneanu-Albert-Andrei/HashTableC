2168b7cf9bd896b24ead9a5b0a11c3a4aedd52af
Andrei ALbert <andrei.albert2708@gmail.com>
2022-07-16 11:32:13 -0400

    Added some code that is broken

--- hashTable.c
+++ hashTable.c
@@ -1,7 +1,11 @@
 #include "hashTable.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-
-void yes(){
-    printf("daeedsgdse\n");
+int create_table(hash_table *h_table , int(*f)(int)) {
+    h_table = malloc(sizeof(hash_table));
+    h_table -> table = malloc(sizeof(hash_node) * 100);
+    h_table -> hash = f;
+    h_table -> size = 10;
+    h_table -> capacity = 100;
 }
--- hashTable.h
+++ hashTable.h
@@ -1,7 +1,21 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
-void yes();
+typedef struct hash_node {
+    hash_node *next , *prev;
+    int value;
+};
+
+typedef struct hash_table {
+    hash_node *table;
+    unsigned int size;
+    unsigned int capacity;
+    int (*hash)(int);
+};
+
+
+int create_table(hash_table *table , int(*f)(int));
+
 
 #endif
 
